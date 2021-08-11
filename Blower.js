376133bb0d36e84d918b746733d3afa107957990
PrabnoorCODING <86729692+PrabnoorCODING@users.noreply.github.com>
2021-08-11 12:28:17 -0400

    Add files via upload

---
+++ Blower.js
@@ -0,0 +1,13 @@
+class blower{
+    constructor(x,y,width,height){
+        var options = {
+            restitution: 0.8
+        };
+
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.width = width;
+        this.height = height;
+
+        World.add(world.this.body);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,26 @@
+var blow,blower;
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+ 
+
+  button = createButton("Click to Blow");
+  button.position(width / 2, height -100);
+  button.class("blowButton");
+  button.mousePressed(blow);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  Blower.Show();
+  
+  drawSprites();
+
+
+}
+
+function blow() {
+  Matter.Body.appleForce(ball.body,{x: 0, y: 0}, {x:0, y: 0.05});
+}
